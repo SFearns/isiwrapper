49362c2835cadff48e213b71c1395237d2b6358e
sfearns <sfearns@hotmail.com>
2015-02-17 16:05:28 +0000

    Sample code to re-sync the cluster time with a domain controller

---
+++ Samples/Isilon Sync Cluster time with a DC.ps1
@@ -0,0 +1,10 @@
+﻿$Cluster="Isilon"
+$ClusterUserID="root"
+$ClusterPassword="password"
+
+$ttConnected=$null
+$ifsStatus=$null
+
+if (!$ttConnected) {$ttConnected=Connect-IsilonCluster -ClusterName $Cluster -Username $ClusterUserID -Password $ClusterPassword}
+
+Set-IsilonSyncTimeWithDomain -ClusterName $Cluster -Domain ADDOMAIN
