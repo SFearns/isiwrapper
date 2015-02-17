fcc74461315edf81f9734abe6814da1d013bfa18
sfearns <sfearns@hotmail.com>
2015-02-17 23:38:50 +0000

    Sample scripts

---
+++ Samples/Isilon - Save NFS configuration.ps1
@@ -0,0 +1,22 @@
+﻿$ToolName="NFS Configuration"
+$ToolVer="1.0"
+$ToolDate="17th February 2015"
+$ToolAuthor="Stephen Fearns"
+
+# Live Environment
+$Cluster="Isilon"
+$ClusterUserID="root"
+$ClusterPassword="password"
+
+# Count must be greater than 0
+$TodaysDate=Get-Date
+$ReportFolder='C:\WorkFolder\Reports\'
+$ReportFileName=[string](Get-Date -Format yyyyMMdd)+"_"+[string](Get-Date -Format HHmmss)+" - $Cluster - NFS Configuration"
+$XMLReport=$ReportFolder+$ReportFileName+'.xml'
+
+$ifsConnection = Connect-IsilonCluster -ClusterName $Cluster -Username $ClusterUserID -Password $ClusterPassword
+if ($ifsConnected -like "Unable to connect to*") {return $ifsConnected}
+if ($ifsConnected -like "No SSH session found*") {return $ifsConnected}
+
+$t=Get-IsilonListNFSShares -ClusterName $Cluster
+$t | Export-Clixml -Depth 30 -Path $XMLReport
